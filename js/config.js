9e36b3563fafa997eadc6cc8f872af13abe28784
kwcloudconsutling <kwcloudconsulting@gmail.com>
2022-10-22 07:54:53 -0500

    new_config

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_yrjhysmNn', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '77lkiikiq3nbig9pnoq0u4asil', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
